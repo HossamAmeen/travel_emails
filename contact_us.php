33f84b35aa9bcd9f92529d268fdcdbaab5840321
hossam ameen <hosamameen948@gmail.com>
2024-10-16 19:13:45 +0300

    update/test speed

--- contact_us.php
+++ contact_us.php
@@ -25,32 +25,32 @@ $template_data =  $twig->render('contact_us.html.twig', $_POST);
 $file_name = "contract_us";
 $user_name = $_POST['name'];
 
-if (!is_dir("uploads")) {
-    mkdir("uploads", 0777, true);
-}
+// if (!is_dir("uploads")) {
+//     mkdir("uploads", 0777, true);
+// }
 
-if (!is_dir("uploads/" . $file_name)) {
-    mkdir("uploads/" . $file_name, 0777, true);
-}
+// if (!is_dir("uploads/" . $file_name)) {
+//     mkdir("uploads/" . $file_name, 0777, true);
+// }
 
-$user_name = strtolower($user_name);
-if (!is_dir("uploads/" . $file_name . '/' . $user_name)) {
-    mkdir("uploads/" . $file_name . '/' . $user_name, 0777, true);
-}
+// $user_name = strtolower($user_name);
+// if (!is_dir("uploads/" . $file_name . '/' . $user_name)) {
+//     mkdir("uploads/" . $file_name . '/' . $user_name, 0777, true);
+// }
 
-$mpdf = new \Mpdf\Mpdf(['default_font' => 'dejavusans']);
-$mpdf->WriteHTML($template_data);
-$pdf_path = "uploads/" . $file_name . $user_name  . '_' . date('Ymd_His'). '.pdf';
-$mpdf->Output($pdf_path, 'F'); 
+// $mpdf = new \Mpdf\Mpdf(['default_font' => 'dejavusans']);
+// $mpdf->WriteHTML($template_data);
+// $pdf_path = "uploads/" . $file_name . $user_name  . '_' . date('Ymd_His'). '.pdf';
+// $mpdf->Output($pdf_path, 'F'); 
 
-$lastSlashPos = strrpos($_SERVER['REQUEST_URI'] , '/');
-$baseUrl = substr($_SERVER['REQUEST_URI'], 0, $lastSlashPos + 1);
-$downloadLink = $_SERVER['HTTP_HOST'] . $baseUrl . '/' . $pdf_path;
+// $lastSlashPos = strrpos($_SERVER['REQUEST_URI'] , '/');
+// $baseUrl = substr($_SERVER['REQUEST_URI'], 0, $lastSlashPos + 1);
+// $downloadLink = $_SERVER['HTTP_HOST'] . $baseUrl . '/' . $pdf_path;
 
 
-$_POST['download_link'] = $downloadLink;
-$_POST['is_email'] = True;
-$template_data =  $twig->render('contact_us.html.twig', $_POST);
+// $_POST['download_link'] = $downloadLink;
+// $_POST['is_email'] = True;
+// $template_data =  $twig->render('contact_us.html.twig', $_POST);
 
 send_email('contract us', 'Ops@whitecloudsaviation.com', $template_data, $template_data, 'contract_us', $user_name);
 
